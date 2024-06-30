b33a54ab976e051c43731d4e05f16875dbbda204
k3ntd4i <ackar_quentdie48@outlook.com>
2024-06-30 15:17:51 -0500

    Modernizacion del codigo

--- Tartarus_Time.cpp
+++ Tartarus_Time.cpp
@@ -1,59 +1,54 @@
-#include<iostream>
-#include<math.h>
-#include<string.h>
-
-using namespace std;
+#include <iostream>
+#include <cmath>
+#include <string>
 
 int main()
 {
-    long double combinaciones_por_segundo = 0, caracteres_disponibles = 0, longitud_contrasenia = 0, operador_tiempo[5] = {60,60,24,365};
-    long double  a = 0, tiempo = 0;
-    long int tiempo_entero = 0, n = 0;
-    char unidad_tiempo[5][10] = {"segundo", "minuto", "hora", "dia", "anio"};
+    unsigned long long int combinaciones_por_segundo{};
+    unsigned int caracteres_disponibles{};
+    unsigned int longitud_contrasenia{};
 
-    cout<<"[+] Digite la cantidad de caracteres diferentes disponibles para usar: "; cin>>caracteres_disponibles;
-    cout<<"[+] Digite la longitud de la contrasenia: "; cin>>longitud_contrasenia;
-    cout<<"[+] Digite el numero de combinaciones por segundo del ataque: "; cin>>combinaciones_por_segundo;
+    std::cout << "[+] Digite la cantidad de caracteres diferentes disponibles para usar: ";
+    std::cin >> caracteres_disponibles;
 
-    a = pow(caracteres_disponibles, longitud_contrasenia);
-    tiempo = a / combinaciones_por_segundo;
+    std::cout << "[+] Digite la longitud de la contrasenia: ";
+    std::cin >> longitud_contrasenia;
+    
+    std::cout << "[+] Digite el numero de combinaciones por segundo soportados en el ataque: ";
+    std::cin >> combinaciones_por_segundo;
+
+    long double permutacion{ std::pow(caracteres_disponibles, longitud_contrasenia) };
+    long double tiempo{ permutacion / combinaciones_por_segundo }; // segundos
+
+    int operador_tiempo[4] { 60, 60, 24, 365 };
+ 
+    std::string unidad_tiempo[5] { "segundo", "minuto", "hora", "dia", "anio" };
+    int indice_unidad_tiempo{};
 
     for (int i = 0; i < 4; i++)
     {
         if (tiempo >= 60 && i < 2)
         {
             tiempo /= operador_tiempo[i];
-            n = i + 1;
+            indice_unidad_tiempo = i + 1;
         }
         else if (tiempo >= 24 && i < 3)
         {
             tiempo /= operador_tiempo[i];
-            n++;
+            indice_unidad_tiempo++;
         }
         else if (tiempo >= 365)
         {
             tiempo /= operador_tiempo[i];
-            n++;
+            indice_unidad_tiempo++;
         }
     }
 
-    cout<<"\n[+] El tiempo que tomaria una contrasenia de "<<longitud_contrasenia<<" caracteres en ser descubierta es de: ";
+    std::cout
+        << "\n[+] El tiempo que tomaria romper una contrasenia con las condiciones digitadas es de: "
+        << "\n[+] " << tiempo << ' ' << unidad_tiempo[indice_unidad_tiempo];
 
-    if (tiempo > 1)
-    {
-        tiempo_entero = tiempo;
-        tiempo = tiempo_entero;
-    }
-    
-    cout<<"\n[+] "<<tiempo<<" "<<unidad_tiempo[n];
-    if (tiempo != 1)
-    {
-        cout<<"s\n\n";
-    }
-    else
-    {
-        cout<<"\n\n";
-    }
+    (tiempo == 1) ? std::cout << '\n' : std::cout << "s\n";
 
     return 0;
 }
